a0f57ebba935322b7f60f87ae539b432e98668cf
sushmi231 <susmitaroytrishna@gmail.com>
2026-06-29 13:39:56 +0600

    Create I. Welcome for you with Conditions.cpp

---
+++ Competetive Programming/ASSUIT ICPC Prep/Sushmita/SRC/Sheet#1 (Data type-Conditions)/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>> a >> b;
+    //if(a>=b) cout << "Yes" << endl;
+    //cout << "No" << endl;
+    cout << ( a>=b ? "Yes" : "No") << endl;
+
+}
